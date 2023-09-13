9eaf43d41d3711a7864b2dd30185457340931d3e
luis92guimaraes <luis92guimaraes@outlook.com>
2023-09-13 19:48:38 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a image
+  if(html.classList.contains('light')) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute('src', './assets/avatar-light.png')
+    // se tiver sem light mode, manter avatar normal
+    img.setAttribute('src', './assets/avatar.png')
+  }
+
+
+
+}
